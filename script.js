e278c4e77b156efd2349e6c9590a2f9a6cbb4e04
Halil Bilgil <hbilgil@gmail.com>
2022-05-07 16:47:10 +0300

    Edit all files

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// Function that builds a grid in the "container"
+const container = document.getElementById('container');
 
-function createCell(x) {
-    for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
-            $("#container").append("<div class='cell'></div>");
-        };
+/*---new function-----
+allowing to create default grid by 16x16 */
+
+function makeRows(rows, cols) {
+
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.innerText = (c + 1);
+      container.appendChild(cell).className = "grid-item";
     };
-    $(".cell").width(960/x);
-    $(".cell").height(960/x);
-};
-
-// Function that clears the grid
-
-function clearCell(){
-    $(".cell").remove();
-};
-
-// Function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
-function recreateCell(){
-    let y = prompt("How many boxes? e.g. 64x64");
-    clearCell();
-    createCell(y);
-};
+  };
+  
+  makeRows(16, 16);
