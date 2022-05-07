68bcacf8f9b8200601e7e4d612ed939c09d33b89
Halil Bilgil <hbilgil@gmail.com>
2022-05-07 15:57:28 +0300

    Edit script.js for promt function allowing user to choose grid size

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ function createCell(x) {
 function clearCell(){
     $(".cell").remove();
 };
+
+// Function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function recreateCell(){
+    let y = prompt("How many boxes? e.g. 64x64");
+    clearCell();
+    createCell(y);
+};
